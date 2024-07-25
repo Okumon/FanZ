d12766084e36771eacfa2405af7726284efbaf1c
FanZ <95639655+Okumon@users.noreply.github.com>
2024-07-25 19:57:17 +0700

    Add files via upload

---
+++ Energy Simulator.lua
@@ -0,0 +1,19 @@
+--inf rebirths
+local args = {
+   [1] = "Rebirth3",
+   [2] = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999,
+   [3] = "Overworld"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):FireServer(unpack(args))
+
+
+
+--infinte rebirths fantasy world
+local args = {
+   [1] = "Rebirth3",
+   [2] = 999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999,
+   [3] = "Fantasy"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Remote"):FireServer(unpack(args))
